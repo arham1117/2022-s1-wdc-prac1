0ac74d2a946b38235fca26f2a27aa380e85b6e3f
Factos7 <100888270+Factos7@users.noreply.github.com>
2022-03-24 04:36:12 +0000

    3-4

--- task3.js
+++ task3.js
@@ -9,13 +9,23 @@ function mouseCount(){
 let countelements = 0;
 function postContent(){
 
+    let posts = document.createElement('DIV');
+    posts.classList.add("posts");
+
     const date = new Date();
+    let time = document.createElement('P');
+    time.classList.add("post-time");
     document.querySelector(".post-time").innerHTML = date;
+    posts.appendChild(time);
+
     let content = document.getElementById("text").value;
-    document.querySelector(".post-content").innerHTML= content;
 
+    let postcontent = document.createElement('P');
+    postcontent.classList.add("post-content");
+    document.querySelector(".post-content").innerHTML = content;
+    posts.appendChild(postcontent);
 
-
+    document.getElementsByTagName('body').appendChild(posts);
 
     /* const date = new Date();
     const timenode = document.createTextNode(date);
@@ -58,8 +68,23 @@ function newPost(){
     var num = document.getElementsByName("quantity")[0].value;
 
     for (let i = 0 ; i<num ;i++){
-        countelements = countelements + 1;
-        postContent();
+        //countelements = countelements + 1;
+        //postContent();
+        const date = new Date();
+        const timenode = document.createTextNode(date);
+        const time = document.createElement("p");
+        time.appendChild (timenode);
+        time.className = "post-time";
+        document.querySelector("#posts").appendChild(time);
+
+
+
+        var textcontent = document.getElementById("text").value;
+        const node = document.createTextNode(textcontent);
+        const paragraph = document.createElement("p");
+        paragraph.appendChild (node);
+        paragraph.className = "post-content";
+        document.querySelector("#posts").appendChild(paragraph);
     }
 
 }
