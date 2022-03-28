891fa0e605aa176e3b30057fd49444054d16f3af
Factos7 <100888270+Factos7@users.noreply.github.com>
2022-03-28 12:58:41 +0000

    3-4

--- task3.js
+++ task3.js
@@ -1,13 +1,14 @@
+
 let count = 0;
 
-function mouseCount(){
+function mouseCount() {
     count = count + 1;
     document.getElementById("mcount").innerHTML = count;
 
 }
 
 let countelements = 0;
-function postContent(){
+function postContent() {
 
     const date = new Date();
     const time = document.createElement("p");
@@ -22,49 +23,38 @@ function postContent(){
     var textcontent = document.getElementById("text").value;
     const paragraph = document.createElement("p");
     paragraph.innerHTML = textcontent;
+    if (document.getElementById("redoption").checked) {
+        paragraph.style.color = "red" ;
+    }
+    if (document.getElementById("blueoption").checked) {
+        paragraph.style.color = "blue" ;
+    }
+    //paragraph.classList.add('post-content');
     paragraph.classList.add('post-content');
-    //paragraph.className = "post-content";
     document.querySelector("#posts").appendChild(paragraph);
 
-    var elements = document.getElementsByClassName("post-content");
-
-    if(document.getElementById("redoption").checked){
-
-        for(let i = 0 ; i<countelements;i++){
-            elements[countelements].style.color ="red";
-        }
-
-    }
-
-    if(document.getElementById("blueoption").checked){
-        for(let i = 0 ; i<countelements;i++){
-            elements[countelements].style.color ="blue";
-        }
-
-    }
-
+    //var elements = document.getElementsByClassName('post-content');
 
 }
 
-function newPost(){
+function newPost() {
 
     var num = document.getElementsByName("quantity")[0].value;
 
-    for (let i = 0 ; i<num ;i++){
+    for (let i = 0; i < num; i++) {
         countelements = countelements + 1;
         postContent();
     }
 
 }
 
-function hideMain(){
+function hideMain() {
     document.getElementById("main").style.display = "none";
     document.getElementById("menu").style.display = "block";
 
 }
-function hideMenu(){
+function hideMenu() {
     document.getElementById("menu").style.display = "none";
     document.getElementById("main").style.display = "block";
 
 }
-
