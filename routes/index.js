7e0c2ae8d46e9779397ead8207d4d603e1d7a2c3
Factos7 <100888270+Factos7@users.noreply.github.com>
2022-03-30 07:53:00 +0000

    3-1

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+const datearray=[];
+datearray[0]= '';
+var counter = 0;
+router.get('/last.txt', function (req, res, next) {
+  //let d = Date.now();
+  res.send(String(datearray[counter]));
+  const date = new Date();
+  datearray[counter+1]= date;
+  counter++;
+
 });
 
 module.exports = router;
