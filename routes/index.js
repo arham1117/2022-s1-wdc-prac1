581cc99a21566ce66d9200e88d2f6b50e1e8b08d
Factos7 <100888270+Factos7@users.noreply.github.com>
2022-03-31 12:04:29 +0000

    4-4

--- routes/index.js
+++ routes/index.js
@@ -118,12 +118,12 @@ router.get('/log.json', function (req, res, next) {
 });
 
 router.get('/contact.ajax', function (req, res, next) {
-  const email = `<a href= + "https://www.nyan.cat/"> + email + </a>`;
+  const email = `<a href= "https://www.nyan.cat/"> email </a>`;
   res.send(email);
 });
 
 router.get('/search.ajax', function (req, res, next) {
-  const search = `<input> + </input> + <button> + search + '</button>`;
+  const search = `<input> </input>  <button> search </button>`;
   res.send(search);
 });
 
