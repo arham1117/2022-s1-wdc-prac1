892eab93e7da32d65ce512f34b081b4cf7dceeca
Factos7 <100888270+Factos7@users.noreply.github.com>
2022-04-01 06:38:36 +0000

    test

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -101,7 +101,7 @@ function contentcheck(){
 }
 
 
-setInterval(gallery,10000);
+//setInterval(gallery,10000);
 
 function gallery(){
 
@@ -109,14 +109,15 @@ function gallery(){
 
     xhttp.onload = function() {
         const images = JSON.parse(this.responseText);
-        //var img = document.createElement("IMG");
-        //img.setAttribute("src", "/images/" + images.uri);
-        //img.setAttribute("alt", images.description);
-        //document.body.appendChild(img);
-        //var para = document.createElement("p");
-        document.getElementById("pic").src = "/images/" + images.uri;
-        document.getElementById("pic").alt = images.description;
-        document.getElementById("para").innerHTML = images.description;
+        var img = document.createElement("IMG");
+        img.setAttribute("src", "/images/" + images.uri);
+        img.setAttribute("alt", images.description);
+        document.body.appendChild(img);
+        var para = document.createElement("p");
+        document.body.appendChild(para);
+        //document.getElementById("pic").src = "/images/" + images.uri;
+        //document.getElementById("pic").alt = images.description;
+        //document.getElementById("para").innerHTML = images.description;
     };
 
     xhttp.open("GET", '/images.json' );
