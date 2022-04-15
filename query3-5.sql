c792dfc17367dc8467d1d0488fd93e8a546e1172
Factos7 <100888270+Factos7@users.noreply.github.com>
2022-04-15 08:45:05 +0000

    final

--- query3-5.sql
+++ query3-5.sql
@@ -1,1 +1,1 @@
-SELECT given_name, family_name, mark  FROM Students INNER JOIN Enrolments  ON Students.student_id = Enrolments.student_id WHERE mark < 50;
+SELECT given_name, family_name, mark  FROM Students INNER JOIN Enrolments  ON Students.student_id = Enrolments.student_id WHERE subject_code LIKE "COMP SCI 2000%" and mark < 50;
