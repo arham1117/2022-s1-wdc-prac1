9ed02f1e7d87f4acf05f6f974c8db0363dc2ae50
Factos7 <100888270+Factos7@users.noreply.github.com>
2022-05-26 08:42:31 +0000

    1

--- query1-2.sql
+++ query1-2.sql
@@ -1,2 +1,2 @@
-SELECT * FROM actor
+SELECT first_name,last_name FROM actor
 ORDER BY last_name;
--- query1-4.sql
+++ query1-4.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT address
 FROM address
-INNER JOIN customer
-      ON address.address_id = customer.address_id
+INNER JOIN store
+      ON address.address_id = store.address_id
 INNER JOIN inventory
-      ON customer.store_id = inventory.store_id
+      ON store.store_id = inventory.store_id
 WHERE film_id = 918;
